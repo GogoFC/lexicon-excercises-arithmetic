1d87abcd25f8736d13663820076a047acb4181d9
Goran Muminovic <goranmuminovic@gmail.com>
2023-11-15 07:41:11 +0100

    done

---
+++ src/main/java/se/lexicon/Main.java
@@ -0,0 +1,22 @@
+package se.lexicon;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner numbers = new Scanner(System.in);
+        System.out.println("Enter two numbers to be calculated!");
+        double number1 = numbers.nextDouble();
+        double number2 = numbers.nextDouble();
+        double sum = number1 + number2;
+        double division = number2 / number1;
+        double subtraction = number1 - number2;
+        double multiplication = number1 * number2;
+
+        System.out.println("The sum of numbers " + number1 + " and " + number2 + " = " +sum);
+        System.out.println(number1 + " times " + number2 + " is " +multiplication);
+        System.out.println(number1 + " divided by " + number2 + " = " +division);
+        System.out.println(number1 + " minus " + number2 + " is " +subtraction);
+    }
+
+}
